76691bca5479c773a51090d924abf7235eec8adc
Voleminh23 <115992040+Voleminh23@users.noreply.github.com>
2024-11-05 12:12:33 +0700

    Revert "Le minh"

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace DACN
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new FormBanHang());
+            Application.Run(new FormDangNhap());
         }
     }
 }
