f9a477a3f0bb96e47b7a8c2cc9015894c8e0d385
voleminh233 <voleminh233@gmail.com>
2024-11-18 22:10:39 +0700

    a

---
+++ DAO/CongNoDAO.cs
@@ -0,0 +1,31 @@
+﻿using DACN.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACN.DAO
+{
+    public class CongNoDAO
+    {
+        private static CongNoDAO instance;
+        public static CongNoDAO Instance
+        {
+            get { if (instance == null) instance = new CongNoDAO(); return CongNoDAO.instance; }
+            private set { CongNoDAO.instance = value; }
+        }
+        public CongNoDAO() { }
+
+        public DataTable (DateTime tungay, DateTime denngay)
+        {
+            string tungay1 = tungay.ToString("yyyy-MM-dd");
+            string denngay1 = tungay.ToString("yyyy-MM-dd");
+            string query = "sp_SelectAll_HD_ByTime @ngayTu , @ngayDen ";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query, new object[] { tungay1, denngay1 });
+            return data;
+        }
+
+    }
+}
---
+++ DTO/CongNoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACN.DTO
+{
+    public class CongNoDTO
+    {
+
+        public decimal tienNhap { get; set; }
+        public decimal tienXuat { get; set; }
+        public decimal ThanhTien { get; set; }
+        public CongNoDTO() { }
+    }
+}
